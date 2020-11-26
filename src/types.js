ed5cb1711997ccf786dce3ee52e024f0f64fb9ef
rezaa91 <rezaa91@hotmail.co.uk>
2020-11-26 00:18:49 +0000

    added basic types

---
+++ src/types.js
@@ -0,0 +1,17 @@
+const syntaxKind = {
+  beginKeyword: 0,
+  endKeyword: 1,
+  scaneofKeyword: 2,
+  plusOp: 3,
+  minusOp: 4,
+  whitespace: 5,
+  initialiser: 6,
+  semiColon: 7,
+  intLiteral: 8,
+  identifier: 9,
+  comment: 10
+}
+
+module.exports = {
+  syntaxKind
+}
