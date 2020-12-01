99c873695cdd5210357ffe77d5f702d6f8a381ca
rezaa91 <rezaa91@hotmail.co.uk>
2020-12-01 19:39:02 +0000

    further unit tests added to scanner

--- tests/scanner.test.js
+++ tests/scanner.test.js
@@ -1,7 +1,7 @@
 const {scanner} = require('../src/compiler/scanner');
 
 describe('scanner', function() {
-  test('scanner returns stream of tokens with valid input', function() {
+  test('returns stream of tokens with valid input', function() {
     const validInput = `
       begin A := 310 ;
       end SCANEOF
@@ -14,5 +14,34 @@ describe('scanner', function() {
     } catch (error) {
       console.error(error);
     }
+  });
+
+  test('no error thrown if identifier value followed by a space', function() {
+    const validInput = `A := 310 B := 200`;
+    const tokenLength = 6;
+
+    const result = scanner(validInput);
+    expect(result.length).toBe(tokenLength);
+  });
+
+  test('no error thrown if identifier value followed by line break', function() {
+    const validInput = `A := 310
+      B := 200`;
+    const tokenLength = 6;
+
+    const result = scanner(validInput);
+    expect(result.length).toBe(tokenLength);
+  })
+
+  test('throws error if invalid token found', function() {
+    const invalidInput = `@`;
+
+    expect(() => scanner(invalidInput)).toThrow(`Unexpected token: "@"`);
+  });
+
+  test('throws error if no space following identifier value', function() {
+    const invalidInput = `A := 310B`;
+
+    expect(() => scanner(invalidInput)).toThrow(`Unrecognised character B`);
   });
 });
