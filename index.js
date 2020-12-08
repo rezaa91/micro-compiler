d94d77e698ed595d96b006819a427026ba10dfe3
rezaa91 <rezaa91@hotmail.co.uk>
2020-12-08 19:57:59 +0000

    Basic generator added

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ const {getSymbolTable} = require('./src/compiler/symbol_table');
   const input = await fs.readFileSync(filename).toString();
 
   const tokens = scanner(input);
+  console.log('\n\nTOKENS:');
   console.dir(tokens, {depth: null});
 
   const ast = parser(tokens);
+  console.log('\n\nABSTRACT SYNAX TREE:');
   console.dir(ast, {depth: null});
 
   /** DEBUG */
-  console.log(getSymbolTable());
+  // console.log('SYMBOL TABLE');
+  // console.log(getSymbolTable());
 
   const output = generator(ast);
+  console.log('\n\nOUTPUT:');
   console.log(output);
 })();
--- src/compiler/generator.js
+++ src/compiler/generator.js
@@ -1,5 +1,13 @@
 function generator(ast) {
+  return ast.body.reduce((acc, item) => {
+    switch (item.type) {
+      case 'IDENTIFIER':
+        acc += `var ${item.identifier} = ${item.value};\n`;
+        break;
+    }
 
+    return acc;
+  }, '');
 }
 
 module.exports = {
