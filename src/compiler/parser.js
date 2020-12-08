7196332747ba2d7523c96a3021b3c6a85485fcef
rezaa91 <rezaa91@hotmail.co.uk>
2020-12-08 19:57:51 +0000

    Parser now generates a simple AST

--- src/compiler/parser.js
+++ src/compiler/parser.js
@@ -55,6 +55,7 @@ function expression() {
 
 
 function statement(type) {
+  const node = {};
   const currentToken = getCurrentToken();
 
   switch (type) {
@@ -67,29 +68,44 @@ function statement(type) {
         // TODO - bug here! We want to store the full expression value; e.g: A := B + 12 - 1;
         //                                                                        ^   ^^   ^
         setSymbolValue(currentToken.symbolId, peek().value); // peek the value to store, e.g. A := 10
+        node.identifier = currentToken.value;
+        node.value = peek().value;
+      } else {
+        node.value = getCurrentToken();
       }
 
+      node.type = 'IDENTIFIER';
+
+      // TODO - store expression in ast
       expression();
       match(characters.semiColon, getNextToken().value);
       break;
     default:
       throw new Error(`unrecognised token: "${currentToken}"`);
   }
+
+  return node;
 }
 
 function statementList() {
-  while(true) {
+  const node = [];
+
+  while(pointer < lexemes.length) {
     const token = getNextToken();
 
     switch (token.type) {
       case syntaxKind.identifier:
-        statement(token.type);
+        const statementNode = statement(token.type);
+        node.push(statementNode);
         break;
       default: // return once we have finished parsing all statements
         pointer--; // decrement as no match
-        return;
+
+        return node;
     }
   }
+
+  return node;
 }
 
 function program() {
@@ -97,16 +113,20 @@ function program() {
    * match: begin <statementList> end
    */
   match(keywords.beginKeyword, getNextToken().value);
-  statementList();
+  const statementListNode = statementList();
   match(keywords.endKeyword, getNextToken().value);
+
+  return [...statementListNode];
 }
 
 function systemGoal() {
   /**
    * match: <program> SCANEOF
    */
-  program();
+  const programNode = program();
   match(keywords.scaneofKeyword, getNextToken().value); // assert program ends with SCANEOF
+
+  return programNode;
 }
 
 function parser(tokens) {
@@ -117,7 +137,29 @@ function parser(tokens) {
    */
   lexemes.push(...tokens);
 
-  systemGoal();
+  const node = {
+    type: 'Program',
+    body: []
+  }
+
+  const programBody = systemGoal();
+  node.body.push(...programBody);
+
+  /**
+   *  {
+   *    type: 'Program',
+   *    body: [
+   *      {
+   *        type: 'IDENTIFIER',
+   *        identifier: 'varName',
+   *        value: 'value'
+   *      },
+   *      {},
+   *      ...
+   *    ]
+   *  }
+   */
+  return node;
 }
 
 module.exports = {
